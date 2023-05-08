b11fe07353353ca536daccaa29c0f14ea721af2f
sam <sam.rodriguez@motor-ai.com>
2023-05-08 12:15:24 +0200

    add thresholds as arguments for generate_boxes.py

--- generate_boxes.py
+++ generate_boxes.py
@@ -98,24 +98,25 @@ def get_annotation_from_xml(xml_path):
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', help='input folder')
-    parser.add_argument('-o', help='output folder')
-    parser.add_argument('-p', help='text prompt', default="traffic light.")
+    parser.add_argument('-i','--input-folder', help='input folder with images')
+    parser.add_argument('-o','--output-folder', help='output folder where labels are stored')
+    parser.add_argument('-p','--prompt', help='text prompt specifying objects to detect', default="traffic light.")
+    parser.add_argument('-t','--text-threshold', help='threshold for the text prompt', default="0.3")
+    parser.add_argument('-b','--box-threshold', help='threshold for the box ', default="0.25")
+
+
     args = vars(parser.parse_args())
 
-    image_path = args['i']
-    output_path = args['o']
-
-    # image_path = '/home/sam/work/tasks/traffic_sign_detection/GroundingDINO/data'
-    # output_path = '/home/sam/work/tasks/traffic_sign_detection/labels'
-    TEXT_PROMPT = args['p']
+    image_path = args['input_folder']
+    output_path = args['output_folder']
+    TEXT_PROMPT = args['prompt']
+    TEXT_TRESHOLD = float(args['text_threshold'])
+    BOX_TRESHOLD = float(args['box_threshold'])
 
     os.makedirs(output_path, exist_ok=True)
     weights_path = "weights/groundingdino_swint_ogc.pth"
     model_path = "models/GroundingDINO_SwinT_OGC.py"
     model = load_model(model_path, weights_path)
-    BOX_TRESHOLD = 0.25
-    TEXT_TRESHOLD = 0.3
     # img_dims = 1900, 1200, 3
 
     for file in tqdm(os.listdir(image_path)):
@@ -137,7 +138,8 @@ if __name__ == '__main__':
 
         write_bbox_to_xml(bbox_annotation=boxes, labels=phrases, logits=logits, 
                           output_file=label_path, filename=file, source=image_path, 
-                          img_dims=img_dims, box_threshold=BOX_TRESHOLD, text_threshold=TEXT_TRESHOLD,
+                          img_dims=img_dims, box_threshold=BOX_TRESHOLD, 
+                          text_threshold=TEXT_TRESHOLD,
                           text_prompt=TEXT_PROMPT)
         
         # boxes, phrases, logits = get_annotation_from_xml(label_path)
